cfe38f97908a3de0e31b5c4a10d7345085eb1b20
Eido Askayo <eido.askayo@gmail.com>
2022-08-05 15:30:35 +0300

    Add polls detail template incl. custom 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
 
@@ -16,7 +16,13 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404(f"Question {question_id} not exists")
+    else:
+        context = {'question': question}
+        return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
