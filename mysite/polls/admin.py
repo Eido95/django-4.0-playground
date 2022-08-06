ecf1220f5ec7e9baa76516290195c6fe33f769af
Eido Askayo <eido.askayo@gmail.com>
2022-08-06 18:20:03 +0300

    Improve polls admin question admin model fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from polls.models import Question
 
+
 # Register your models here.
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
